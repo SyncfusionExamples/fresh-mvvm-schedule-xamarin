86e51051a1990a8b7a4cc3c93969af9669e8509b
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-03-17 16:37:25 +0530

    XAMARIN-33983 - Sample committed.

---
+++ ScheduleXamarin/ScheduleXamarin/ScheduleXamarin/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using FreshMvvm;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace ScheduleXamarin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var page = FreshPageModelResolver.ResolvePageModel<SchedulerPageModel>();
+            var basicNavContainer = new FreshNavigationContainer(page);
+            MainPage = basicNavContainer;
+        }
+    }
+}
